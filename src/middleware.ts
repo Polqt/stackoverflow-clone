8d705281db30ff4860b4136e761f03481bef95c3
Polqt <poyhidalgo@gmail.com>
2025-04-06 17:25:52 +0800

    feat: add middleware to handle database and storage initialization

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import getOrCreateDB from './models/server/dbSetup';
+import getOrCreateStorage from './models/server/storageSetup';
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+    await Promise.all([
+        getOrCreateDB(),
+        getOrCreateStorage()
+    ])
+
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  // Match all request paths except for the ones starts with `/api/` or `/static/`
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+};
