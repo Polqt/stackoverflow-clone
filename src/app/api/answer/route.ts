64decdfdd89bf14ab7c81fff3d2f8186fea96d80
Polqt <poyhidalgo@gmail.com>
2025-04-08 23:07:48 +0800

    chore: improve error handling and code formatting in answer routes

--- src/app/api/answer/route.ts
+++ src/app/api/answer/route.ts
@@ -9,29 +9,80 @@ export async function POST(request: NextRequest) {
     const { questionId, answer, authorId } = await request.json();
 
     if (!questionId || !answer || !authorId) {
-        return NextResponse.json(
-            { error: 'Missing required fields' },
-            { status: 400 },
-        )
+      return NextResponse.json(
+        { error: 'Missing required fields' },
+        { status: 400 },
+      );
     }
 
-    const response = await databases.createDocument(db, answerCollection, ID.unique(), {
+    const response = await databases.createDocument(
+      db,
+      answerCollection,
+      ID.unique(),
+      {
         content: answer,
         authorId: authorId,
         questionId: questionId,
-    })
+      },
+    );
 
     // Increase author's reputation by 1
-    const prefs = await users.getPrefs<UserPrefs>(authorId)
+    const prefs = await users.getPrefs<UserPrefs>(authorId);
 
     await users.updatePrefs(authorId, {
       reputation: Number(prefs.reputation) + 1,
     });
 
     return NextResponse.json(response, {
-        status: 201,
-    })
+      status: 201,
+    });
+  } catch (error: unknown) {
+    return NextResponse.json(
+      {
+        error:
+          error instanceof Error ? error?.message : 'Error creating answer',
+      },
+      {
+        status:
+          error instanceof Error && 'status' in error
+            ? (error as { status: number }).status
+            : 500,
+      },
+    );
+  }
+}
 
+export async function DELETE(request: NextRequest) {
+  try {
+    const { answerId } = await request.json();
+
+    if (!answerId) {
+      return NextResponse.json(
+        { error: 'Missing required fields' },
+        { status: 400 },
+      );
+    }
+
+    const answer = await databases.getDocument(db, answerCollection, answerId);
+
+    if (!answer) {
+      return NextResponse.json({ error: 'Answer not found' }, { status: 404 });
+    }
+
+    const response = await databases.deleteDocument(
+      db,
+      answerCollection,
+      answerId,
+    );
+
+    // Decrease author's reputation by 1
+    const prefs = await users.getPrefs<UserPrefs>(answer.authorId);
+
+    await users.updatePrefs(answer.authorId, {
+      reputation: Number(prefs.reputation) - 1,
+    });
+
+    return NextResponse.json({ data: response }, { status: 200 });
   } catch (error: unknown) {
     return NextResponse.json(
       {
