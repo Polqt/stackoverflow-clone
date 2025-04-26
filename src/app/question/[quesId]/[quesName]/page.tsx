e355a5ea2cfaa20870b34c5c66c977b49ec05358
Polqt <poyhidalgo@gmail.com>
2025-04-26 13:58:05 +0800

    chore: enhance question page to include detailed answer metadata and author information

--- src/app/question/[quesId]/[quesName]/page.tsx
+++ src/app/question/[quesId]/[quesName]/page.tsx
@@ -10,8 +10,7 @@ import { users } from '@/models/server/config';
 import { UserPrefs } from '@/store/Auth';
 import { Query } from 'node-appwrite';
 
-
-const Page = async ({ params }: { quesId: string; quesName: string }) => {
+const Page = async ({ params }: { params: { quesId: string; quesName: string } }) => {
   const [question, answers, upvotes, downvotes, comments] = await Promise.all([
     databases.getDocument(db, questionCollection, params.quesId),
     databases.listDocuments(db, answerCollection, [
@@ -38,6 +37,71 @@ const Page = async ({ params }: { quesId: string; quesName: string }) => {
   ]);
 
   const author = await users.get<UserPrefs>(question.authorId);
+  [comments.documents, answers.documents] = await Promise.all([
+    Promise.all(
+      comments.documents.map(async comment => {
+        const author = await users.get<UserPrefs>(comment.authorId);
+        return {
+          ...comment,
+          author: {
+            $id: author.$id,
+            name: author.name,
+            reputation: author.prefs.reputation,
+          },
+        };
+      }),
+    ),
+    Promise.all(
+      answers.documents.map(async answer => {
+        const [author, upvotes, downvotes] = await Promise.all([
+          users.get<UserPrefs>(answer.authorId),
+          databases.listDocuments(db, voteCollection, [
+            Query.equal('typeId', answer.$id),
+            Query.equal('type', 'answer'),
+            Query.equal('voteStatus', 'upvoted'),
+            Query.limit(1),
+          ]),
+          databases.listDocuments(db, voteCollection, [
+            Query.equal('typeId', answer.$id),
+            Query.equal('type', 'answer'),
+            Query.equal('voteStatus', 'downvoted'),
+            Query.limit(1),
+          ]),
+          databases.listDocuments(db, commentCollection, [
+            Query.equal('type', 'answer'),
+            Query.equal('typeId', answer.$id),
+            Query.orderDesc('$createdAt'),
+          ]),
+        ]);
+
+        comments.documents = await Promise.all(
+          comments.documents.map(async comment => {
+            const author = await users.get<UserPrefs>(comment.authorId);
+            return {
+              ...comment,
+              author: {
+                $id: author.$id,
+                name: author.name,
+                reputation: author.prefs.reputation,
+              },
+            };
+          }),
+        );
+
+        return {
+          ...answer,
+          comments,
+          upvotesDocuments: upvotes,
+          downvotesDocuments: downvotes,
+          author: {
+            $id: author.$id,
+            name: author.name,
+            reputation: author.prefs.reputation,
+          },
+        };
+      }),
+    ),
+  ]);
 
   return (
     <div>
